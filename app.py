89c502bc631a7b4a6acf9fd0815938bfa96e7875
omarabdelhady802 <159005365+omarabdelhady802@users.noreply.github.com>
2024-02-05 11:47:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, request, f
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timedelta
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import safe_str_cmp
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///limo.db'
