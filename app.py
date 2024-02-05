60a0ebeee89476e4ebb6d3f264082e2c86bd8b10
omarabdelhady802 <159005365+omarabdelhady802@users.noreply.github.com>
2024-02-05 11:33:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -836,6 +836,6 @@ def logout():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    # with app.app_context():
-    #     db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
